7af33bf0fe057cbc6c03fb52e9326f108183c2f7
torotoki <hiroyoshi.komat@gmail.com>
2013-06-14 02:20:29 +0900

    Update installer with CoreNLP v1.3.5

--- install_stanford_models.sh
+++ install_stanford_models.sh
@@ -5,9 +5,9 @@
 # Before running this scripts, download CoreNLP and copy the stanford-corenlp-X.Y.Z-models.jar in this directory.
 #
 # If you use Windows and cannot run this script as is, this essentially copies the jar file to:
-# ~/.m2/repository/edu/stanford/nlp/stanford-corenlp-models/1.3.4/stanford-corenlp-models-1.3.4.jar 
+# ~/.m2/repository/edu/stanford/nlp/stanford-corenlp-models/1.3.5/stanford-corenlp-models-1.3.5.jar
 #
 
-CORENLP_VER=1.3.4
+CORENLP_VER=1.3.5
 
 mvn install:install-file -DartifactId=stanford-corenlp-models -DgroupId=edu.stanford.nlp -Dversion=$CORENLP_VER -Dpackaging=jar -Dfile=./stanford-corenlp-$CORENLP_VER-models.jar
